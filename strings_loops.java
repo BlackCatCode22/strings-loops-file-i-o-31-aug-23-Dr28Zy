d696c6cad6aa0e639768a120e8eb3819435c16fd
Dr28Zy <141769669+Dr28Zy@users.noreply.github.com>
2023-08-31 12:13:51 -0700

    Add files via upload

---
+++ strings_loops.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class strings_loops {
+    public static void main(String[] args) {
+
+        String phrase = "The quick brown fox jumped over the lazy dog";
+
+        System.out.println(phrase.substring(9, phrase.indexOf("jumped")));
+
+        String[] friends = {"Jim ", "Angela ", "Oscar ", "Dwight "};
+        for (int i = 0; i < friends.length; i++) {
+            System.out.print(friends[i]);
+
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+            numbers.add(3);
+            numbers.add(7);
+            numbers.add(2);
+
+            for(int a : numbers) {
+                System.out.print(a);
+            }
+                {
+
+                };
+
+
+            }
+        }
+    }
